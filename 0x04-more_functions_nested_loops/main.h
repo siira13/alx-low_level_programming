cf68c1569077ca00dc3a15a1dc23abbf254bcac6
Fikirte M. Manaye <fikirross2010@gmail.com>
2021-09-03 12:22:19 +0300

    task 2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,6 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);*/
 int _isupper(int c);
-
+int mul(int a, int b);
 
 #endif /* MAIN_H */
