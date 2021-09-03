bf2ecd64002745f1d5cc474bc205bd36a6f3bebf
Fikirte M. Manaye <fikirross2010@gmail.com>
2021-09-03 12:01:35 +0300

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h "
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+    int _isupper(int c);
+    {
+    char c;
+
+switch (c)
+{
+case 'A':
+return (0);
+case 'a':
+return  (0);
+ }
+    }
