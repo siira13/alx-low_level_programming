d17e974fd3ae68aa28302708f30f5024f3bace58
Fikirte M. Manaye <fikirross2010@gmail.com>
2021-09-07 16:41:47 +0300

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -22,3 +22,16 @@ char *strcpy(char *dest, char *src)
 	dest[l] = '\0';
 	return (dest);
 }
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+		j++;
+	return (dest);
+}
+
